df2a6272789316857ce3468c89f028f12c9ee422
buğra uysal <uysalbugra134@gmail.com>
2024-07-03 03:49:09 +0300

    git hub a eklendi ve kodlar bitti

---
+++ klbul.py
@@ -0,0 +1,5 @@
+var =("bu gün türkiye avusturya maçını barda izledik güzeldi sonra eve geldik ve yattık")
+if "evlen" in var:
+    print("evet, bu kelime cümlede var")
+else:
+    print("hayır , bu cümlede yok")
